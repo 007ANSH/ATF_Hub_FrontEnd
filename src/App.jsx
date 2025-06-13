f3c3f94a3d932e9b7d8b04ec5db07d8736c110a3
Gusain <ansh.gusain@accenture.com>
2025-06-13 16:11:42 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './Pages/Login'
-import HomePage from './Pages/HomePage'
-import LoginPage from './Pages/Login'
-// import { MsalAuthenticationTemplate } from "@azure/msal-react";
-// const ProtectedPage = () => {
-//     return (
-//         <MsalAuthenticationTemplate>
-//             <div>
-//                 <h1>Protected Content</h1>
-//             </div>
-//         </MsalAuthenticationTemplate>
-//     );
-// };
-function App() {
-  return(
-    <>
-     <Routes>
-            <Route path="/" element={<LoginPage />} />
-            {/* <Route path="/protected" element={<ProtectedPage />} /> */}
-            <Route path="/home" element={<HomePage />} />
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
 
-        </Routes> 
-    </>
-  )
+// Only import the one LoginPage component
+import LoginPage from './Pages/Login';
+import HomePage from './Pages/HomePage';
+
+// MSAL React
+import { MsalAuthenticationTemplate } from "@azure/msal-react";
+
+function App() {
+  return (
+    <Routes>
+      {/* Public login page */}
+      <Route path="/" element={<LoginPage />} />
+
+      {/* Protected home page */}
+      <Route
+        path="/home"
+        element={
+          <MsalAuthenticationTemplate>
+            <HomePage />
+          </MsalAuthenticationTemplate>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
